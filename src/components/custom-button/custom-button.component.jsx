608d3d1ab1f740d7f551a4feaf2be01bb435cb5f
adetoro <torokoga@gmail.com>
2020-12-17 16:41:54 +0100

    updated shop data with new data, updated cart utils to grop items in cart reducer

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 import './custom-button.styles.scss';
 
-const CustomButton = ({ children, isGoogleSignIn, ...othrProbs}) => (
-    <button className={`${isGoogleSignIn ? 'google-sign-in' : '' } custom-button`} {...othrProbs}> 
+const CustomButton = ({ children, isGoogleSignIn, inverted, ...othrProbs}) => (
+    <button className={`${inverted ? 'inverted' : '' } ${isGoogleSignIn ? 'google-sign-in' : '' } custom-button`} {...othrProbs}> 
         {children}
     </button>
 );
